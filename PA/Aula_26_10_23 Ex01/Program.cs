1ed839d9a1e4a33975cabda13dcc9fb838f2e5f1
Aluno <aluno@etec.sp.gov.br>
2023-10-26 20:48:56 -0300

    Aula 26_10_23 Exercícios do While sobre parcelas

---
+++ PA/Aula_26_10_23 Ex01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula_26_10_23_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+
+            do
+            {
+                Console.WriteLine($"2 x {contador} = {2 * contador}");
+                //contador = contador + 1;
+                contador++;
+            } while (contador <= 10);
+        }
+    }
+}
---
+++ PA/Aula_26_10_23_Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula_26_10_23_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorFinanciado, valorParcela;
+            int qtdparcela, contador = 1;
+
+            Console.Write("Qual o valor parcelado?: ");
+            valorFinanciado = float.Parse(Console.ReadLine());
+
+            Console.Write("Em quantas vezes você deseja parcelar: ");
+            qtdparcela = int.Parse(Console.ReadLine());
+
+            valorParcela = valorFinanciado / qtdparcela;
+
+            DateTime data = DateTime.Now.Date;
+
+            do
+            {
+                //Console.WriteLine($"{contador}ª parcela: {valorFinanciado / qtdparcela:} ");
+                Console.WriteLine($"{contador}ª parcela ({data}) {valorParcela:0.00} ");
+                contador++;
+                data = data.AddMonths(1);
+            } while (contador <= qtdparcela);
+
+        }
+    }
+}

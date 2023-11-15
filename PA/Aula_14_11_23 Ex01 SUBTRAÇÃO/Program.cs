a66922a0cc1e4a56c0b1741296bfd3cd443d524d
Aluno <aluno@etec.sp.gov.br>
2023-11-14 22:51:15 -0300

    Aula_14_11_23 Exercicios numeros impar e subtração

---
+++ PA/Aula_14_11_23 Ex01 SUBTRAÇÃO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula_14_11_23_Ex01_SUBTRAÇÃO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sub1, sub2;
+             // string sub3 = "S";
+
+            // while (sub3.ToUpper() == "S")
+
+            for (int subtração = 1; subtração <=3; subtração++)
+            {
+
+                Console.WriteLine("Digite o primeiro número da subtração: ");
+                sub1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo número da subtração: ");
+                sub2 = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                Console.WriteLine($"{sub1} - {sub2} = {sub1 - sub2}");
+
+            }
+
+
+        }
+    }
+}
---
+++ PA/Aula_14_11_23 Ex02 NUMEROS IMPAR ATE N/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_14_11_23_Ex02_NUMEROS_IMPAR_ATE_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite um número para que seja destacado os números ímpares dele: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i =1 ; i<= num; i++)
+            //for (int i = 1; 1 <= num; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i } não é impar");
+                }
+                else
+                {
+                    Console.WriteLine($"{i } é ímpar");
+                }
+                 
+
+
+            }
+        }
+    }
+}
